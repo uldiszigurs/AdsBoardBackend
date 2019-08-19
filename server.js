bfe5317f154b435ce043c2d28b3095d74f08a5c5
uldiszigurs <uldis.zzigurs@gmail.com>
2019-08-19 13:15:53 +0300

    Added packages, added windson logger

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import session from 'express-session';
+import mongo from 'connect-mongo';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import './utilities/dotenv';
+
---
+++ utilities/logger.js
@@ -0,0 +1,58 @@
+import { createLogger, format, transports } from 'winston';
+/* 
+Winston is package for logging; Look for transports & levels
+https://www.npmjs.com/package/winston 
+*/
+
+const { combine, timestamp, label, printf, colorize, align, splat } = format;
+
+const logsFormat = printf(info => {
+  const ts = info.timestamp.slice(0, 19).replace('T', ' ');
+  return `${ts} ${info.level} ${info.label}: ${info.message}`;
+});
+
+const Logger = fileName => {
+  let level = 'info';
+  if (process.env.DEBUG) {
+    level = 'debug';
+  }
+  const consoleLogger = new transports.Console({
+    level,
+    format: combine(
+      colorize(),
+      timestamp(),
+      align(),
+      label({ label: fileName }),
+      splat(),
+      logsFormat,
+    ),
+  });
+  const fileLogger = new transports.File({
+    filename: `logs/combined.log`,
+    level: 'debug',
+    maxsize: 5242880, // 5 mb
+    maxFiles: 2,
+    format: combine(timestamp(), align(), label({ label: fileName }), splat(), logsFormat),
+  });
+  const transportsArray = [fileLogger];
+  if (process.env.NODE_ENV !== 'test') {
+    transportsArray.push(consoleLogger);
+  }
+  const logger = createLogger({
+    transports: transportsArray,
+  });
+  return logger;
+};
+//FIXME: in case of error of exporting
+//module.exports = Logger;
+export {Logger};
+
+// { 
+//  Logging levels : 
+//   error: 0,
+//   warn: 1,
+//   info: 2,
+//   verbose: 3,
+//   debug: 4,
+//   silly: 5
+// }
